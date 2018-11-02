7be1b6adc99408582c61d2a36f203bb913ac3876
Evan <evan.rypel@gmail.com>
2018-11-02 12:41:48 -0700

    Arrays

---
+++ HackerRankPractices/src/Node/js/Arrays.js
@@ -0,0 +1,57 @@
+/**
+ * 
+ */
+'use strict';
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.trim().split('\n').map(string => {
+        return string.trim();
+    });
+    
+    main();    
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+/**
+*   Return the second largest number in the array.
+*   @param {Number[]} nums - An array of numbers.
+*   @return {Number} The second largest number in the array.
+**/
+function getSecondLargest(nums) {
+    // Complete the function
+    
+    // sort descending
+    nums.sort(function(x, y) { return x < y;});
+    
+    var secondLargest;
+    for(var i = 0; i < nums.length; i++)
+        {
+            if(nums[i] != nums[i+1])
+                {
+                    secondLargest = nums[i+1];
+                    break;
+                }
+        }
+    
+    return secondLargest;
+}
+
+function main() {
+    const n = +(readLine());
+    const nums = readLine().split(' ').map(Number);
+    
+    console.log(getSecondLargest(nums));
+}
